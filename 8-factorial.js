6ed632ffabacd9110927f09550c6ad9fad3bfa89
visaj-555 <visaj@sevensquaretech.com>
2025-02-04 22:00:48 +0530

    SOLVED 9th problem

--- 8-factorial.js
+++ 8-factorial.js
@@ -1,3 +1,19 @@
+// EASY APPROACH
+
+function factorial(n) {
+  let result = 1;
+
+  for (let i = n; i >= 1; i--) {
+    result *= i; // Multiply instead of adding
+  }
+
+  return result;
+}
+
+let number = 5;
+console.log("Factorial of", number, "is:", factorial(number));
+
+// RECURSIVE APPROACH
 function factorial(n) {
   if (n === 0 || n === 1) return 1;
   return n * factorial(n - 1);
