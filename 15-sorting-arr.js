42344c9dd94fc8b29074b4f33e946fa09512dc14
visaj-555 <visaj@sevensquaretech.com>
2025-02-10 20:58:15 +0530

    solved sorting problem

---
+++ 15-sorting-arr.js
@@ -0,0 +1,15 @@
+function sortArray(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+let input = [3, 1, 4, 1, 5, 9];
+console.log(sortArray(input));
