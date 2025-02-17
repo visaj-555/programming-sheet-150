e54a8b1c560959d52ad0a85008cd06b007eb4852
visaj-555 <visaj@sevensquaretech.com>
2025-02-17 21:45:14 +0530

    Solved sum of even and odd numbers

--- 22-odd-average.js
+++ 22-odd-average.js
@@ -1,13 +1,13 @@
-function sumOfEvenNumbers(start, end) {
+function sumOfOddNumbers(start, end) {
   let sum = 0;
 
   for (let i = start; i <= end; i++) {
-    if (i % 2 === 0) {
+    if (i % 2 === 1) {
       sum += i;
     }
   }
 
-  console.log("Sum of even numbers:", sum);
+  console.log("Sum of odd numbers:", sum);
 }
 
 sumOfEvenNumbers(1, 10);
