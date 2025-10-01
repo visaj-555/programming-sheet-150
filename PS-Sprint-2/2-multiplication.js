55079a0cb8ebc4ed26658cc57e9ce03e1619ab35
visaj-cilans <visaj.cilans@gmail.com>
2025-10-01 22:06:39 +0530

    01-10-2025

--- PS-Sprint-2/2-multiplication.js
+++ PS-Sprint-2/2-multiplication.js
@@ -1,3 +1,9 @@
-function multiplication(start, end) {}
+function multiplication(start, end) {
+  for (let i = start; i <= end; i++) {
+    for (let j = 1; j <= 10; j++) {
+      console.log(i + "x" + j + " = " + i * j);
+    }
+  }
+}
 
 console.log(multiplication(2, 4));
---
+++ PS-Sprint-2/3-sum-series.js
@@ -0,0 +1,9 @@
+function sumOfSeries(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {}
+}
+
+sumOfSeries(A);
+
+// Example: Input: n = 4;
+// Output: 2.083333;
