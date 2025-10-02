ef098856208b787ff56c97b82edf63a7cd56d47b
visaj-cilans <visaj.cilans@gmail.com>
2025-10-02 18:06:20 +0530

    02-10-2025

--- PS-Sprint-2/3-sum-series.js
+++ PS-Sprint-2/3-sum-series.js
@@ -1,9 +1,13 @@
 function sumOfSeries(n) {
   let sum = 0;
-  for (let i = 0; i <= n; i++) {}
+  for (let i = 1; i < n; i++) {
+    sum += 1 / i;
+  }
+
+  return sum + 1;
 }
 
-sumOfSeries(A);
+console.log(sumOfSeries(4));
 
 // Example: Input: n = 4;
 // Output: 2.083333;
