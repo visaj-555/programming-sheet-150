255403cf706525ccabddd9d084a67fdd6a8edad2
visaj-cilans <visaj.cilans@gmail.com>
2025-10-26 17:36:45 +0530

    26-10-2025

--- PS-Sprint-3/30-cross-pattern.js
+++ PS-Sprint-3/30-cross-pattern.js
@@ -1,3 +1,14 @@
-function crossPattern(n) {}
+function crossPatternWithDiagonals(n) {
+  let totalRows = 2 * n - 1;
 
-crossPattern(5);
+  for (let i = 0; i < totalRows; i++) {
+    let spaces = i < n ? i : totalRows - i - 1;
+    let stars = n - spaces;
+
+    let row = " ".repeat(spaces);
+    row += "* ".repeat(stars).trim();
+    console.log(row);
+  }
+}
+
+crossPatternWithDiagonals(5);
